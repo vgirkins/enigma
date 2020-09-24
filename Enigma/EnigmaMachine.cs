ca898aab9d0c77bfd37708836ffec6cfa6694238
vgirkins <28710650+vgirkins@users.noreply.github.com>
2020-09-23 22:09:08 -0600

    Added gitignore; allowed scramblers to be changed out

--- Enigma/EnigmaMachine.cs
+++ Enigma/EnigmaMachine.cs
@@ -8,18 +8,13 @@ namespace Enigma
 {
     public class EnigmaMachine
     {
-        private Scrambler scrambler1;
-        private Scrambler scrambler2;
-        private Scrambler scrambler3;
+        private Scrambler[] scramblers;
         private Reflector reflector;
         private Plugboard plugboard;
 
-        public EnigmaMachine(Scrambler scrambler1, Scrambler scrambler2, Scrambler scrambler3, Reflector reflector, Plugboard plugboard)
+        public EnigmaMachine(Scrambler[] scramblers, Reflector reflector, Plugboard plugboard)
         {
-            // TODO make generic number scramblers
-            this.scrambler1 = scrambler1;
-            this.scrambler2 = scrambler2;
-            this.scrambler3 = scrambler3;
+            this.scramblers = scramblers;
             this.reflector = reflector;
             this.plugboard = plugboard;
         }
@@ -35,31 +30,35 @@ namespace Enigma
                 // Run the letter through the plugboard
                 cryptedLetter = plugboard.Process(letter);
                 // Run the letter through the scramblers
-                cryptedLetter = scrambler3.Scramble(scrambler2.Scramble(scrambler1.Scramble(cryptedLetter)));
+                foreach (var scrambler in this.scramblers)
+                {
+                    cryptedLetter = scrambler.Scramble(cryptedLetter);
+                }
                 // Reflect it
                 cryptedLetter = reflector.Reflect(cryptedLetter);
                 // Run it back through the scramblers
-                cryptedLetter = scrambler1.ReverseScramble(scrambler2.ReverseScramble(scrambler3.ReverseScramble(cryptedLetter)));
+                foreach (var scrambler in this.scramblers.Reverse())
+                {
+                    cryptedLetter = scrambler.ReverseScramble(cryptedLetter);
+                }
                 // Run it back through the plugboard
                 cryptedLetter = plugboard.Process(cryptedLetter);
 
                 cryptedMessage += cryptedLetter;
+
                 // Rotate the scramblers
-                scrambler1.Rotate();
-                if (i % EnigmaHelper.NumLetters == 0)
+                foreach (var scrambler in this.scramblers)
                 {
-                    scrambler2.Rotate();
-                    if (i % (EnigmaHelper.NumLetters ^ 2) == 0)
-                    {
-                        scrambler3.Rotate();
-                    }
+                    if (i % Math.Pow(EnigmaHelper.NumLetters, Array.IndexOf(this.scramblers, scrambler)) == 0)
+                        scrambler.Rotate();
                 }
             }
 
             // Reset scramblers to their original positions
-            scrambler1.Reset();
-            scrambler2.Reset();
-            scrambler3.Reset();
+            foreach (var scrambler in this.scramblers)
+            {
+                scrambler.Reset();
+            }
 
             return cryptedMessage;
         }
